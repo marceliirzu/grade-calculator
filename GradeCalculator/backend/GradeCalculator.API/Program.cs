354c87a40aef4fc090d9fd4fa9a7ce927d68c40c
marceliirzu <marceliirzu@gmail.com>
2025-12-19 05:01:51 -0600

    Update Program.cs

--- GradeCalculator/backend/GradeCalculator.API/Program.cs
+++ GradeCalculator/backend/GradeCalculator.API/Program.cs
@@ -60,8 +60,8 @@ builder.Services.AddCors(options =>
                 "http://localhost:3000",
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
-                "https://getyourgpa.com",
-                "https://www.getyourgpa.com"
+                "https://calcyourgpa.com",
+                "https://www.calcyourgpa.com"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
